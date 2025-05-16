4685dff84d765c46dd9e5fcc7747ddcd0b3e68a5
Ano <useranonyme317@gmail.com>
2025-05-16 22:51:35 +0100

    CRUD init version, get post working

--- menuManager/menu/urls.py
+++ menuManager/menu/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import MenuItemListView
+from .views import MenuItemListView, MenuItemDetailView, MenuItemUpdateView, MenuItemDeleteView
+
 
 urlpatterns = [
     path('menu/', MenuItemListView.as_view(), name='list-menu-items'),
+    path('menu/<int:id>/', MenuItemDetailView.as_view(), name='menu-detail'),
+    path('menu/<int:id>/', MenuItemUpdateView.as_view(), name='menu-update'),
+    path('menu/<int:id>/', MenuItemDeleteView.as_view(), name='menu-delete')
 ]
--- menuManager/menu/views.py
+++ menuManager/menu/views.py
@@ -2,10 +2,30 @@
 
 # Create your views here.
 
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 from .models import MenuItem
 from .serializers import MenuItemSerializer
 
-class MenuItemListView(ListAPIView):
+# This one is for select all / and POST 
+class MenuItemListView(ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+
+# specific ID menu/ID
+class MenuItemDetailView(RetrieveAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    lookup_field = 'id'
+
+# Update query with ID menu/ID
+class MenuItemUpdateView(UpdateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    lookup_field = 'id'
+
+
+#  DELETE query with ID menu/ID
+class MenuItemDeleteView(DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    lookup_field = 'id'
